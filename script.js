06e5c8c457ad080a8d6c1d9a35dcdc89ad21a28e
Veliko <Kardziev>
2023-08-02 18:17:50 +0200

    adding buttons in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
